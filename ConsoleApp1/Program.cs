125a7aefd1d9e93279dc2d58d4ffe2903158d63a
kn1171998 <2001160303@ittc.edu.vn>
2020-08-16 14:22:17 +0700

    *

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using NUnitTestProject1;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo2Test d = new Demo2Test();
+            d.SetUp();
+            d.demo2();            
+        }
+    }
+}
